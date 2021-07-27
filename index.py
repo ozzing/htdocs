ae6e9c9116c0930403736f320dd13c91cb8c28b4
주어진사랑 <63996052+ozzing@users.noreply.github.com>
2021-07-28 03:03:34 +0900

    Python

---
+++ index.py
@@ -0,0 +1,29 @@
+#! python
+print("Content-Type: text/html\n")
+import cgi
+form = cgi.FieldStorage()
+if 'id' in form:
+    pageId = form.getvalue("id")
+    description = open('data/'+pageId, 'r', encoding="UTF8").read()
+else:
+    pageId='Welcome'
+    description = 'Hello, WEB'
+print('''
+<!doctype html>
+<html>
+<head>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.py">WEB</a></h1>
+  <ol>
+    <li><a href="index.py?id=HTML">HTML</a></li>
+    <li><a href="index.py?id=CSS">CSS</a></li>
+    <li><a href="index.py?id=JavaScript">JavaScript</a></li>
+  </ol>
+  <h2>{title}</h2>
+  <p>{desc}</p>
+</body>
+</html>
+'''.format(title=pageId, desc=description))
---
+++ syntax/string4.py
@@ -0,0 +1,5 @@
+# positional formating
+print('to {}. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim apple veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. {} Duis aute irure dolor in {} reprehenderit apple computer in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui {} officia deserunt mollit anim id est laborum.'.format('ozing', 12, 'ozing', 'ozing'))
+
+# named placeholder
+print('to {name}. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim apple veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. {age:d} Duis aute irure dolor in {name} reprehenderit apple computer in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui {name} officia deserunt mollit anim id est laborum.'.format(name = 'ozing', age = 12))
